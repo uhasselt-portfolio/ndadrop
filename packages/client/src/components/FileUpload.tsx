87e5a9646a5f18b4136a7f6d7693d89dd3f6ded3
Michiel Swaanen <michiel.swaanen@hotmail.com>
2022-10-11 09:21:47 +0200

    :sparkles: Upload revamp

--- packages/client/src/components/FileUpload.tsx
+++ packages/client/src/components/FileUpload.tsx
@@ -1,63 +1,73 @@
+import { Upload } from 'lucide-preact';
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useState, useRef } from 'preact/hooks';
 
 interface Props {
-	uploadFile : (file : File) => boolean
+	uploadFile: (file : File) => boolean
+  fileSelected: (file : File) => void
 }
 
 // Component to select a file to upload
 const FileUpload = (props: Props) => {
-    const [file, sefFile] = useState<File>();
+  const [file, sefFile] = useState<File | null>(null);
 
-    // On file select (from the pop up)
-    const onFileChange = (event : any) => {
-    
-        // Update the state
-        sefFile(event.target.files[0]);
-    };
+  const pickerRef = useRef<HTMLInputElement>(null);
 
-    // On file upload (click the upload button)
-    const onFileUpload = () => {
-        // Call the function passed as a prop
-        if (file)
-            props.uploadFile(file);
-    };
+  // On file select (from the pop up)
+  const onFileChange = (event: any) => {
+    console.log("File selected", event.target.files[0]);
 
-    const showCurrentFile = () => {
-        if (file) {
-            return (
-              <div>
-                <h2>File Details:</h2>
-                <p>File Name: {file.name}</p>
-                <p>File Type: {file.type}</p>
-                <p>
-                  Last Modified:{" "}
-                  {file.lastModified.toString()}
-                </p>
-              </div>
-            );
-          } else {
-            return (
-              <div>
-                <br />
-                <h4>Choose before Pressing the Upload button</h4>
-                <br />
-              </div>
-            );
-          }
+    // Update the state
+    sefFile(event.target.files[0]);
+    props.fileSelected(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    console.log("handleUpload", file, pickerRef.current);
+
+    if (file === null) {
+      if (pickerRef.current) pickerRef.current.click();
+    } else {
+      props.uploadFile(file);
     }
+  }
 
-	return(
+  const showCurrentFile = () => {
+    if (file) {
+      return (
         <div>
-            <div>
-                <input type="file" onChange={onFileChange} />
-                {showCurrentFile()}
-                <button onClick={onFileUpload}>
-                  Upload!
-                </button>
-            </div>
-		</div>
-	)
+          <h2>File Details:</h2>
+          <p>File Name: {file.name}</p>
+          <p>File Type: {file.type}</p>
+          <p>Last Modified: {file.lastModified.toString()}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+          <br />
+        </div>
+      );
+    }
+  };
+
+  // return (
+  //   <div>
+  //     <div>
+  //       {showCurrentFile()}
+  //       <button onClick={onFileUpload}>Upload!</button>
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    <div onClick={handleUpload}>
+      <input class='hidden' ref={pickerRef} type="file" onChange={onFileChange} />
+      <Upload color="white" size={16}/>
+    </div>
+  )
 }
 
 export default FileUpload;
